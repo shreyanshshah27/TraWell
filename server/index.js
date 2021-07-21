ce2e9300adcbdad968a70edde5eae39a7bcb00e6
Shreyansh Shah <shreyansh_shah@yahoo.com>
2021-07-21 14:14:16 +0530

    script

--- server/index.js
+++ server/index.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const cors = require('cors')
-// require('dotenv').config();
-const app = express()
-const port = 8000;
-const db = require('./config/mongoose');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3333;
+const db = require("./config/mongoose");
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/',require('./routes'))
+app.use("/", require("./routes"));
 
-module.exports = app.listen(port,function(err){
-    if(err){
-        console.log(`Error in running the server ${err}`);
-    }else{
-        console.log(`Server is up and running on port ${port}`);
-    } 
-})
+module.exports = app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error in running the server ${err}`);
+  } else {
+    console.log(`Server is up and running on port ${port}`);
+  }
+});
